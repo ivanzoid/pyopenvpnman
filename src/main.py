c537e782e83015c7a283ca967d58cd7396a8d19d
Ivan Zezyulya <ivan@zezyulya.com>
2010-02-21 22:43:00 +0600

    Added list view with sample connections

--- src/main.py
+++ src/main.py
@@ -11,42 +11,42 @@ class MainWindow(wx.Frame):
         wx.Frame.__init__(self, parent, id, title)
 
         # init toolbar
-        #tsize = (15,15)
         self.toolbar = self.CreateToolBar(wx.TB_HORIZONTAL | wx.NO_BORDER | wx.TB_FLAT)
 
-        #artBmp = wx.ArtProvider.GetBitmap
-        
         self.toolbar.AddSimpleTool(
-            id_CONNECT, wx.Bitmap('images/connect.bmp', wx.BITMAP_TYPE_BMP), 'Connect')
+            id_CONNECT, wx.Bitmap('images/connect.png', wx.BITMAP_TYPE_PNG), 'Connect')
         self.toolbar.AddSimpleTool(
-            id_DISCONNECT, wx.Bitmap('images/disconnect.bmp', wx.BITMAP_TYPE_BMP), 'Disconnect')
+            id_DISCONNECT, wx.Bitmap('images/disconnect.png', wx.BITMAP_TYPE_PNG), 'Disconnect')
         self.toolbar.AddSimpleTool(
-            id_EDITCFG, wx.Bitmap('images/editcfg.bmp', wx.BITMAP_TYPE_BMP), 'Edit config')
+            id_EDITCFG, wx.Bitmap('images/editcfg.png', wx.BITMAP_TYPE_PNG), 'Edit config')
         self.toolbar.AddSimpleTool(
-            id_VIEWLOG, wx.Bitmap('images/viewlog.bmp', wx.BITMAP_TYPE_BMP), 'View log')
+            id_VIEWLOG, wx.Bitmap('images/viewlog.png', wx.BITMAP_TYPE_PNG), 'View log')
         
         self.toolbar.Realize()
-
-
-    # This method is called by the System when the window is resized,
-    # because of the association above.
-    def OnSize(self, event):
-        size = event.GetSize()
-        self.sizeCtrl.SetValue("%s, %s" % (size.width, size.height))
-
-        # tell the event system to continue looking for an event handler,
-        # so the default handler will get called.
-        event.Skip()
-
-    # This method is called by the System when the window is moved,
-    # because of the association above.
-    def OnMove(self, event):
-        pos = event.GetPosition()
-        self.posCtrl.SetValue("%s, %s" % (pos.x, pos.y))
+        
+        # init list view
+        
+        self.list = wx.ListCtrl(self, -1, style=wx.LC_REPORT)
+        
+        self.list.InsertColumn(0, '') # image
+        self.list.InsertColumn(1, 'Connection')
+        self.list.InsertColumn(2, 'Status')
+        
+        self.list.InsertStringItem(0, 'Item 1')
+        self.list.InsertStringItem(1, 'Item 2')
+        self.list.InsertStringItem(2, 'Item 3')
+        
+        self.list.SetStringItem(0, 1, 'Connection 1')
+        self.list.SetStringItem(0, 2, 'Disconnected')
+        self.list.SetStringItem(1, 1, 'Connection 2')
+        self.list.SetStringItem(1, 2, 'Disconnected')
+        self.list.SetStringItem(2, 1, 'Connection 3')
+        self.list.SetStringItem(2, 2, 'Connected')
+                
 
 class App(wx.App):
     def OnInit(self):
-        wnd = MainWindow(None, -1, "This is a test")
+        wnd = MainWindow(None, -1, "OpenVPN Connection Manager")
         wnd.Show(True)
         self.SetTopWindow(wnd)
         return True
