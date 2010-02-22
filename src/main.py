17b39ec7a9469a3614a6e38c15633e1fb921d911
Ivan Zezyulya <ivan@zezyulya.com>
2010-02-22 14:48:40 +0600

    Better list view look

--- src/main.py
+++ src/main.py
@@ -26,20 +26,40 @@ class MainWindow(wx.Frame):
         self.toolbar.Realize()
         
         # init list view
+
+        self.imgs = wx.ImageList(24, 24, mask=True)
+        self.imgs.Add(wx.Bitmap('images/connected24.png', wx.BITMAP_TYPE_PNG))
+        self.imgs.Add(wx.Bitmap('images/notconnected24.png', wx.BITMAP_TYPE_PNG))
         
-        self.list = wx.ListCtrl(self, -1, style=wx.LC_REPORT)
+        self.list = wx.ListCtrl(self, -1, style=(wx.LC_REPORT | wx.LC_SINGLE_SEL | wx.LC_HRULES | wx.LC_VRULES))
+        self.list.SetImageList(self.imgs, wx.IMAGE_LIST_SMALL)
         
-        self.list.InsertColumn(0, '') # image
-        self.list.InsertColumn(1, 'Connection')
+        self.list.InsertColumn(0, '')
+        self.list.InsertColumn(1, 'Name')
         self.list.InsertColumn(2, 'Status')
         
-        self.list.InsertStringItem(0, 'Item 1')
-        self.list.InsertStringItem(1, 'Item 2')
-        self.list.InsertStringItem(2, 'Item 3')
+#        self.list.InsertImageItem(index=0, imageIndex=0)
+#        self.list.InsertImageItem(index=1, imageIndex=1)
+#        self.list.InsertImageItem(index=2, imageIndex=0)
+        
+#        self.list.InsertImageStringItem(index=0, label='', imageIndex=0)
+#        self.list.InsertImageStringItem(index=0, label='', imageIndex=0)
+#        self.list.InsertImageStringItem(index=0, label='', imageIndex=0)
+        
+        self.list.InsertStringItem(0, '')
+        self.list.InsertStringItem(1, '')
+        #self.list.InsertStringItem(2, '')
         
         for i in range(len(self.ovpnnames)):
-            self.list.SetStringItem(i, 1, self.ovpnnames[i])
-            self.list.SetStringItem(i, 2, 'Disconnected')
+            #self.list.SetStringItem(index=i, col=0, label='', imageId=1)
+            #self.list.SetItemImage(i, 0)
+            self.list.SetStringItem(index=i, col=0, imageId=1, label='')
+            self.list.SetStringItem(index=i, col=1, label=self.ovpnnames[i])
+            self.list.SetStringItem(index=i, col=2, label='Disconnected')            
+            #self.list.SetStringItem(index=i, col=2, label='Disconnected')
+            
+        self.list.SetColumnWidth(0, wx.LIST_AUTOSIZE)
+        self.list.SetColumnWidth(1, wx.LIST_AUTOSIZE)
         
     def getConnList(self):
         files = os.listdir('C:\\Program Files\\OpenVPN\\config')
@@ -53,7 +73,7 @@ class App(wx.App):
     def OnInit(self):
         wnd = MainWindow(None, -1, "OpenVPN Connection Manager")
         wnd.Show(True)
-        self.SetTopWindow(wnd)
+        #self.SetTopWindow(wnd)
         return True
 
 if __name__ == '__main__':
