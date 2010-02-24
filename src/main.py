5e0a30a4e60ea01dbc6832bae7f96a83a0f605c7
Ivan Zezyulya <ivan@zezyulya.com>
2010-02-24 16:10:03 +0600

    Authorization using dialog seems to work

--- src/main.py
+++ src/main.py
@@ -3,7 +3,6 @@ import os
 import subprocess
 import socket, asyncore, asynchat
 import random
-from string import join
 
 id_CONNECT = wx.NewId()
 id_DISCONNECT = wx.NewId()
@@ -11,10 +10,52 @@ id_EDITCFG = wx.NewId()
 id_VIEWLOG = wx.NewId()
 id_REFRESH = wx.NewId()
 
+class AuthDlg(wx.Dialog):
+    def __init__(self, parent):
+        wx.Dialog.__init__(self, parent, -1, 'Authorization required')#, style=(wx.DEFAULT_DIALOG_STYLE | wx.RESIZE_BORDER))
+
+        userlabel = wx.StaticText(self, -1, "Username: ")
+        passlabel = wx.StaticText(self, -1, "Password: ")
+
+        self.username = wx.TextCtrl(self, -1, "", size=(200,-1))
+        self.password = wx.TextCtrl(self, -1, "", size=(200,-1), style=wx.TE_PASSWORD)
+
+        grid = wx.FlexGridSizer(2, 2, vgap=5, hgap=10)
+
+        grid.Add(userlabel, 0, wx.ALIGN_CENTER_VERTICAL | wx.ALIGN_LEFT)
+        grid.Add(self.username, 1, wx.ALIGN_CENTER_VERTICAL)
+        grid.Add(passlabel, 0, wx.ALIGN_CENTER_VERTICAL | wx.ALIGN_LEFT)
+        grid.Add(self.password, 1, wx.ALIGN_CENTER_VERTICAL)
+
+        h2 = self.CreateButtonSizer(wx.OK | wx.CANCEL)
+
+        v = wx.BoxSizer(wx.VERTICAL)
+        v.AddSpacer(7)
+        v.Add(grid, 0, 0)
+        v.AddSpacer(10)
+        v.Add(h2, 0, wx.ALIGN_CENTER_HORIZONTAL)
+        v.AddSpacer(7)
+
+        # sizer only for spacers
+        h = wx.BoxSizer(wx.HORIZONTAL)
+        h.AddSpacer(7)
+        h.Add(v)
+        h.AddSpacer(7)
+
+        self.SetSizer(h)
+        h.Fit(self)
+    
+
+def escapePassword(password):
+    result = password.replace('\\', '\\\\').replace('"', '\\"').replace(' ', '\\ ')
+    return result
+    
+
 class ManagementInterfaceHandler(asynchat.async_chat):
-    def __init__(self, addr, port):
+    def __init__(self, mainwnd, addr, port):
         asynchat.async_chat.__init__(self)
         #print 'ManagementInterfaceHandler construct'
+        self.mainwnd = mainwnd
         self.port = port
         self.buf = ''
         self.set_terminator('\n')
@@ -31,6 +72,15 @@ class ManagementInterfaceHandler(asynchat.async_chat):
         
     def found_terminator(self):
         #print 'found_terminator ({0}) buf: "{1}"'.format(self.port, self.buf)
+        if self.buf[0:9] == '>PASSWORD':
+            #self.mainwnd.CredentialsRequired(self.port)
+            authdlg = AuthDlg(self.mainwnd)
+            authdlg.ShowModal()
+            username = authdlg.username.GetValue()
+            password = authdlg.password.GetValue()
+            authdlg.Destroy()
+            self.send('username "Auth" {0}\n'.format(username))
+            self.send('password "Auth" "{0}"\n'.format(escapePassword(password)))
         self.buf = ''
 
 def connStatusString(status):
@@ -171,7 +221,7 @@ class MainWindow(wx.Frame):
                           '--management-query-passwords',
                           '--management-log-cache', '200'],
                           cwd=self.ovpnconfigpath)
-        self.connsocks[index] = ManagementInterfaceHandler('127.0.0.1', port)
+        self.connsocks[index] = ManagementInterfaceHandler(self, '127.0.0.1', port)
         self.connstatus[index] = 1
         self.updateConnection(index)
         self.updateToolbar(index)
@@ -181,6 +231,7 @@ class MainWindow(wx.Frame):
         index = self.list.GetFocusedItem()
         if index == -1:
             return
+        self.connsocks[index].send('signal SIGTERM\n')
         self.connstatus[index] = 0
         self.updateConnection(index)
         self.updateToolbar(index)
